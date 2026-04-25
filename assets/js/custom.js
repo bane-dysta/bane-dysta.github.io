b3d24c388ece1d984c5539d6478a4cf88f22c3d4
bane-dysta <943284406@qq.com>
2026-04-25 21:04:34 +0800

    7.5.0 -- _includes/head.html _includes/js-selector.html _layouts/default.html

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -91,3 +91,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll("a[href^='http']");
+  links.forEach(link => {
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "noopener noreferrer");
+  });
+});
+
+
